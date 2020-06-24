e5088771ae499c2689f7d8a09d95b81bc4bfac29
Ezeko <ezeko2017@gmail.com>
2020-06-24 04:12:31 +0100

    bug
    add csrf token to dashboard and talk forms

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,6 +2,7 @@
 @if (count($talks) > 0)
 @foreach ($talks as $talk)
 <form method="post" action="/talk/attendee/add">
+    {{@csrf_field()}}
     <input type="hidden" name="attendee_id" value="{{session()->get('user_id')}}" required/>
     <input type="hidden" name="talk_id" value="{{$talk->talk_id}}" required/>
     <p>{{$talk->talk_title}} </p>
