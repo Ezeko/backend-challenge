d4f57757d1adef5b5bab6ccd94e78b372b2171ab
Ezeko <ezeko2017@gmail.com>
2020-06-24 04:20:44 +0100

    bug
    make talk_title , title

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
     {{@csrf_field()}}
     <input type="hidden" name="attendee_id" value="{{session()->get('user_id')}}" required/>
     <input type="hidden" name="talk_id" value="{{$talk->talk_id}}" required/>
-    <p>{{$talk->talk_title}} </p>
+    <p>{{$talk->title}} </p>
     <input type="submit" value="Add Talk" />
 </form>
 @endforeach
